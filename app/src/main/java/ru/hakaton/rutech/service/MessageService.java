c70b310514124e6a8ccdbfe8df8e3c3ffbc971d1
NightDoctor <NightDoctor@.github.com>
2020-06-06 15:07:21 +0300

    Gazman

--- app/src/main/java/ru/hakaton/rutech/service/MessageService.java
+++ app/src/main/java/ru/hakaton/rutech/service/MessageService.java
@@ -3,7 +3,10 @@ package ru.hakaton.rutech.service;
 import ru.hakaton.rutech.model.Message;
 import ru.hakaton.rutech.model.Room;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -29,12 +32,31 @@ public class MessageService {
     }
 
     public Message send(Room room, Message message) {
-        //TODO добавить сообщение в мапу ключ комната значение список сообщений
+        //TODO добавить сообщение в мапу ключ комната значение список сообщений есть ли такая комната
+        if(room == null) {//кто и что написал
+            List<Message> list = new ArrayList<>();
+            list.add(message);
+            roomMessageMap.put(new Room(), list);
+        } else  {
+            List<Message> list = new ArrayList<>();
+            list.add(message);
+            roomMessageMap.put(room, list);
+        }
+
+        for(int i = 0; i < 3; i++) {
+            List<Message> list = new ArrayList<>();
+            list.add(new Message());
+            roomMessageMap.put(room, list);
+        }
         return message;
     }
 
     public List<Message> getAll(Room room) {
         //todo вернуть все сообщения в комнате из мапы
-        return null;
+        roomMessageMap.get(room);
+        if(roomMessageMap.get(room) == null) {
+            return roomMessageMap.put(room, new ArrayList<Message>());
+        }
+        return roomMessageMap.get(room);
     }
 }
